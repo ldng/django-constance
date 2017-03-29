c2d209754855466a76bc45c5bec59b210adb25a8
ldng <ludovic+gh@danigo.net>
2017-03-29 10:13:14 +0200

    Only apply permissions if auth app is installed

--- constance/apps.py
+++ constance/apps.py
@@ -1,6 +1,6 @@
 from django.db.models import signals
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class ConstanceConfig(AppConfig):
     name = 'constance'
@@ -16,17 +16,22 @@ class ConstanceConfig(AppConfig):
         Creates a fake content type and permission
         to be able to check for permissions
         """
+        from django.contrib.contenttypes.models import ContentType
 
         if ContentType._meta.installed:
-            from django.contrib.contenttypes.models import ContentType
             content_type, created = ContentType.objects.using(using).get_or_create(
                 app_label='constance',
                 model='config',
             )
 
+        if 'auth' not in settings.INSTALLED_APPS:
+            return
+
+        from django.contrib.auth.models import Permission
+
         if Permission._meta.installed:
-            from django.contrib.auth.models import Permission
             permission, created = Permission.objects.using(using).get_or_create(
                 name='Can change config',
                 content_type=content_type,
                 codename='change_config')
+
